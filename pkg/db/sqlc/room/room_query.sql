507d0917f0e5fbfec4ad81781f00361d5e67a439
the_void1 <iamvinod2001@gmail.com>
2025-03-01 23:06:09 +0530

    started rooms

--- pkg/db/sqlc/room/room_query.sql
+++ pkg/db/sqlc/room/room_query.sql
@@ -1,1 +1,19 @@
--- room
+-- name: CreateRoom :one
+INSERT INTO room (
+    id, 
+    room_code, 
+    room_owner, 
+    room_members, 
+    room_chat, 
+    leaderboard, 
+    room_meta, 
+    room_lock, 
+    is_active, 
+    is_deleted, 
+    created_on, 
+    updated_on, 
+    created_by, 
+    updated_by
+) 
+VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, NOW(), NOW(), $11, $12)
+RETURNING *;
