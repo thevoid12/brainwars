5ea48210970e28446df8ced5f88f7ad3fc7bf544
thevoid12 <iamvinod2001@gmail.com>
2025-03-03 16:39:26 +0530

    answer wip

--- pkg/quiz/model/quiz_model.go
+++ pkg/quiz/model/quiz_model.go
@@ -1,1 +1,79 @@
 package model
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type Options struct {
+	ID     uuid.UUID
+	Option string
+}
+type QuestionData struct {
+	Question string
+	Options
+	Answer uuid.UUID // option id: which option is correct
+}
+
+// QuestionReq represents the request to create a question
+type QuestionReq struct {
+	RoomID       uuid.UUID
+	Topic        string
+	QuestionData []*QuestionData
+	CreatedBy    string
+}
+
+// EditQuestionReq represents the request to update a question
+type EditQuestionReq struct {
+	ID           uuid.UUID
+	Topic        string
+	QuestionData []*QuestionData
+	UpdatedBy    string
+}
+
+// Question represents the question model
+type Question struct {
+	ID           uuid.UUID
+	RoomID       uuid.UUID
+	Topic        string
+	QuestionData []*QuestionData
+	CreatedOn    time.Time
+	UpdatedOn    time.Time
+	CreatedBy    string
+	UpdatedBy    string
+}
+
+// AnswerReq represents the request to create an answer
+type AnswerReq struct {
+	RoomID         uuid.UUID
+	UserID         uuid.UUID
+	QuestionID     uuid.UUID
+	QuestionDataID uuid.UUID
+	AnswerOption   int32
+	IsCorrect      bool
+	AnswerTime     time.Time
+	CreatedBy      string
+}
+
+// EditAnswerReq represents the request to update an answer
+type EditAnswerReq struct {
+	ID           uuid.UUID
+	AnswerOption int32
+	IsCorrect    bool
+	AnswerTime   time.Time
+	UpdatedBy    string
+}
+
+// Answer represents the answer model
+type Answer struct {
+	ID           uuid.UUID
+	RoomID       uuid.UUID
+	UserID       uuid.UUID
+	QuestionID   uuid.UUID
+	AnswerOption int32
+	IsCorrect    bool
+	AnswerTime   time.Time
+	CreatedBy    string
+	UpdatedBy    string
+}
--- pkg/quiz/quiz_service.go
+++ pkg/quiz/quiz_service.go
@@ -1,1 +1,199 @@
 package quiz
+
+import (
+	dbpkg "brainwars/pkg/db"
+	"brainwars/pkg/db/dbal"
+	logs "brainwars/pkg/logger"
+	"brainwars/pkg/quiz/model"
+	"context"
+	"encoding/json"
+
+	"github.com/google/uuid"
+	"github.com/jackc/pgx/v5/pgtype"
+)
+
+// CreateQuestion creates a new question in the database
+func CreateQuestion(ctx context.Context, req model.QuestionReq) error {
+	l := logs.GetLoggerctx(ctx)
+
+	json.Unmarshal()
+	params := dbal.CreateQuestionParams{
+		RoomID:       pgtype.UUID{Bytes: req.RoomID, Valid: true},
+		Topic:        pgtype.Text{String: req.Topic, Valid: true},
+		QuestionData: req.QuestionData,
+		CreatedBy:    req.CreatedBy,
+		UpdatedBy:    req.CreatedBy,
+	}
+
+	dbConn, err := dbpkg.InitDB()
+	if err != nil {
+		l.Sugar().Error("Could not initialize database", err)
+		return err
+	}
+	defer dbConn.Db.Close()
+
+	dBal := dbal.New(dbConn.Db)
+	err = dBal.CreateQuestion(ctx, params)
+	if err != nil {
+		l.Sugar().Error("Could not create question in database", err)
+		return err
+	}
+
+	return nil
+}
+
+// UpdateQuestionByID updates a question by its ID
+func UpdateQuestionByID(ctx context.Context, req model.EditQuestionReq) error {
+	l := logs.GetLoggerctx(ctx)
+	params := dbal.UpdateQuestionByIDParams{
+		ID:           pgtype.UUID{Bytes: req.ID, Valid: true},
+		Topic:        pgtype.Text{String: req.Topic, Valid: true},
+		QuestionData: req.QuestionData,
+		UpdatedBy:    req.UpdatedBy,
+	}
+
+	dbConn, err := dbpkg.InitDB()
+	if err != nil {
+		l.Sugar().Error("Could not initialize database", err)
+		return err
+	}
+	defer dbConn.Db.Close()
+
+	dBal := dbal.New(dbConn.Db)
+	err = dBal.UpdateQuestion(ctx, params)
+	if err != nil {
+		l.Sugar().Error("Could not update question in database", err)
+		return err
+	}
+
+	return nil
+}
+
+// ListQuestionsByRoomID lists questions by room ID
+func ListQuestionsByRoomID(ctx context.Context, roomID uuid.UUID) ([]*model.Question, error) {
+	l := logs.GetLoggerctx(ctx)
+	var questionDetails []*model.Question
+	dbConn, err := dbpkg.InitDB()
+	if err != nil {
+		l.Sugar().Error("Could not initialize database", err)
+		return nil, err
+	}
+	defer dbConn.Db.Close()
+
+	dBal := dbal.New(dbConn.Db)
+	questions, err := dBal.ListQuestionsByRoomID(ctx, pgtype.UUID{Bytes: roomID, Valid: true})
+	if err != nil {
+		l.Sugar().Error("Could not list questions in database", err)
+		return nil, err
+	}
+
+	for _, question := range questions {
+		questionDetails = append(questionDetails, &model.Question{
+			ID:           question.ID.Bytes,
+			RoomID:       question.RoomID.Bytes,
+			Topic:        question.Topic.String,
+			QuestionData: string(question.QuestionData),
+			CreatedOn:    question.CreatedOn.Time,
+			UpdatedOn:    question.UpdatedOn.Time,
+			CreatedBy:    question.CreatedBy,
+			UpdatedBy:    question.UpdatedBy,
+		})
+	}
+
+	return questionDetails, nil
+}
+
+// CreateAnswer creates a new answer in the database
+func CreateAnswer(ctx context.Context, req model.AnswerReq) error {
+	l := logs.GetLoggerctx(ctx)
+	params := dbal.CreateAnswerParams{
+		RoomID:         req.RoomID,
+		UserID:         req.UserID,
+		QuestionID:     req.QuestionID,
+		QuestionDataID: req.QuestionDataID,
+		AnswerOption:   req.AnswerOption,
+		IsCorrect:      req.IsCorrect,
+		AnswerTime:     req.AnswerTime,
+		CreatedBy:      req.CreatedBy,
+		UpdatedBy:      req.CreatedBy,
+	}
+
+	dbConn, err := dbpkg.InitDB()
+	if err != nil {
+		l.Sugar().Error("Could not initialize database", err)
+		return err
+	}
+	defer dbConn.Db.Close()
+
+	dBal := dbal.New(dbConn.Db)
+	err = dBal.CreateAnswer(ctx, params)
+	if err != nil {
+		l.Sugar().Error("Could not create answer in database", err)
+		return err
+	}
+
+	return nil
+}
+
+// UpdateAnswer updates an existing answer in the database
+func UpdateAnswer(ctx context.Context, req model.AnswerUpdateReq) error {
+	l := logs.GetLoggerctx(ctx)
+	params := dbal.UpdateAnswerParams{
+		ID:           pgtype.UUID{Bytes: req.ID, Valid: true},
+		AnswerOption: req.AnswerOption,
+		IsCorrect:    req.IsCorrect,
+		AnswerTime:   req.AnswerTime,
+		UpdatedBy:    req.UpdatedBy,
+	}
+
+	dbConn, err := dbpkg.InitDB()
+	if err != nil {
+		l.Sugar().Error("Could not initialize database", err)
+		return err
+	}
+	defer dbConn.Db.Close()
+
+	dBal := dbal.New(dbConn.Db)
+	err = dBal.UpdateAnswer(ctx, params)
+	if err != nil {
+		l.Sugar().Error("Could not update answer in database", err)
+		return err
+	}
+
+	return nil
+}
+
+// ListAnswersByRoomID lists answers by room ID
+func ListAnswersByRoomID(ctx context.Context, roomID uuid.UUID) ([]*model.Answer, error) {
+	l := logs.GetLoggerctx(ctx)
+	var answerDetails []*model.Answer
+	dbConn, err := dbpkg.InitDB()
+	if err != nil {
+		l.Sugar().Error("Could not initialize database", err)
+		return nil, err
+	}
+	defer dbConn.Db.Close()
+
+	dBal := dbal.New(dbConn.Db)
+	answers, err := dBal.ListAnswersByRoomID(ctx, pgtype.UUID{Bytes: roomID, Valid: true})
+	if err != nil {
+		l.Sugar().Error("Could not list answers in database", err)
+		return nil, err
+	}
+
+	for _, answer := range answers {
+		answerDetails = append(answerDetails, &model.Answer{
+			ID:           answer.ID.Bytes,
+			RoomID:       answer.RoomID.Bytes,
+			UserID:       answer.UserID.Bytes,
+			QuestionID:   answer.QuestionID.Bytes,
+			AnswerOption: answer.AnswerOption,
+			IsCorrect:    answer.IsCorrect,
+			AnswerTime:   answer.AnswerTime.Time,
+			CreatedBy:    answer.CreatedBy,
+			UpdatedBy:    answer.UpdatedBy,
+		})
+	}
+
+	return answerDetails, nil
+}
