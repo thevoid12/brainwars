61330349d767f71a6b955534733b9c85c7d8f554
thevoid12 <iamvinod2001@gmail.com>
2025-05-30 16:41:13 +0530

    check for existsing process from the second attempt

--- deployment/bootstrap.sh
+++ deployment/bootstrap.sh
@@ -202,15 +202,15 @@ else
     sudo nginx
 fi
 
-
-
+echo "Stated nginx successfully"
 # Find the PID of any existing brainwars process
-PID=$(pgrep -f "./brainwars")
-
-if [ -n "$PID" ]; then
-    echo "brainwars is already running with PID $PID. Stopping..."
-    kill "$PID"
-    sleep 1  # give it a moment to shut down
+if [ -f "brainwars" ]; then
+    PID=$(pgrep -f "./brainwars")
+    if [ -n "$PID" ]; then
+        echo "brainwars is already running with PID $PID. Stopping..."
+        kill "$PID"
+        sleep 1  # give it a moment to shut down
+    fi
 fi
 
 # start the backend server
